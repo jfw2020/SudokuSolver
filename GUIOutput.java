486ff1ea8406d366733774ac073ebcad349032d5
treyetzel <73486289+treyetzel@users.noreply.github.com>
2020-11-20 22:12:34 -0600

    Add files via upload

---
+++ GUIOutput.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.lang.*;
+import java.util.*;
+public class GUIOutput {
+  static JFrame frame = new JFrame("Sudoku Solver");
+  public static void printBoardGUI(Item[] list, int counter) throws InterruptedException {
+    StringBuilder sb = new StringBuilder();
+    //Thread.sleep(36);
+    sb.append("Iterations: " + counter + "\n" + " ------------------------------" + "\n");
+    for(int i = 0; i < 9; i++) {
+      sb.append(" |  ");
+      for(int j = 0; j < 9; j++) {
+        sb.append(list[i*9 + j].getVal() + " ");
+        if(j % 3 == 2) {
+          sb.append(" |  ");
+        }
+      }
+      sb.append("\n");
+      if(i % 3 == 2) {
+        sb.append(" ------------------------------\n");
+      }
+    }
+    
+    frame.getContentPane().removeAll();
+    JTextArea ta = new JTextArea(sb.toString());
+    ta.setPreferredSize(new Dimension(500, 500));
+    ta.setFont(new Font("Times New Roman", Font.BOLD, 30));
+    ta.setEditable(false);
+    frame.add(ta);
+    frame.pack();
+    frame.setVisible(true);
+  }
+  
+  public static void printSolved(boolean b) {
+    if(!b) {
+      JOptionPane.showMessageDialog(null, "Unsolvable Sudoku");
+    } else {
+      JOptionPane.showMessageDialog(null, "Solved!");
+    }
+  }
+}
